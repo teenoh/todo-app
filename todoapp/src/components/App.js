e9eb06e4a51ab4d007c10fe8a032ee464240fff5
teenoh <valentino@grit.systems>
2017-07-19 17:59:16 +0100

    updated listItem

--- todoapp/src/components/App.js
+++ todoapp/src/components/App.js
@@ -8,11 +8,13 @@ const styles = {
   }
 };
 
-const ListItem = (props) => {
+const ListItem = props => {
   return (
     <div className="list-group-item row">
       <span className="pull-left pointer">+</span>
-      <span style={styles.text}>I want to go to banana island</span>
+      <span style={styles.text}>
+        {props.text}
+      </span>
       <span className="pull-right pointer">-</span>
     </div>
   );
@@ -26,13 +28,13 @@ class App extends React.Component {
       list: []
     };
 
-    this.updateList = this.updateList.bind(this)
+    this.updateList = this.updateList.bind(this);
   }
 
   updateList(item) {
     const newList = [...this.state.list, item];
     this.setState(() => ({ list: newList }));
-    console.log(this.state.list)
+    console.log(this.state.list);
   }
 
   render() {
@@ -41,12 +43,15 @@ class App extends React.Component {
         <center className="row">
           <h3>TO DO LIST APP</h3>
           <br />
-          <Search updateList={this.updateList}/>
+          <Search updateList={this.updateList} />
         </center>
         <br />
 
         <div className="list-group">
-          <ListItem />
+          {this.state.list !== [] &&
+            this.state.list.map((item, index) =>
+              <ListItem key={index} text={item} remove={this.removeItem} />
+            )}
         </div>
       </div>
     );
